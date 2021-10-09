ca94532822a23eda04e17aa6a9e825058cfc42fc
Saurav Gandhi <49108365+sgandhi04@users.noreply.github.com>
2021-10-09 16:24:05 -0700

    Create Linefollower.ino

---
+++ Linefollower.ino
@@ -0,0 +1,64 @@
+#include <Arduino.h>
+#include <Wire.h>
+#include <SoftwareSerial.h>
+
+#include <MeMCore.h>
+
+MeDCMotor motor_9(9);
+MeDCMotor motor_10(10);
+void move(int direction, int speed)
+{
+      int leftSpeed = 0;
+      int rightSpeed = 0;
+      if(direction == 1){
+        	leftSpeed = speed;
+        	rightSpeed = speed;
+      }else if(direction == 2){
+        	leftSpeed = -speed;
+        	rightSpeed = -speed;
+      }else if(direction == 3){
+        	leftSpeed = -speed;
+        	rightSpeed = speed;
+      }else if(direction == 4){
+        	leftSpeed = speed;
+        	rightSpeed = -speed;
+      }
+      motor_9.run((9)==M1?-(leftSpeed):(leftSpeed));
+      motor_10.run((10)==M1?-(rightSpeed):(rightSpeed));
+}
+double angle_rad = PI/180.0;
+double angle_deg = 180.0/PI;
+double speed;
+double linestate;
+MeLineFollower linefollower_4(4);
+
+void setup(){
+    speed = 80;
+}
+
+void loop(){
+    linestate = linefollower_4.readSensors();
+    if(((linestate)==(0))){
+        move(1,speed);
+    }else{
+        if(((linestate)==(1))){
+            move(3,speed);
+        }else{
+            if(((linestate)==(2))){
+                move(4,speed);
+            }else{
+                move(2,speed);
+            }
+        }
+    }
+    _loop();
+}
+
+void _delay(float seconds){
+    long endTime = millis() + seconds * 1000;
+    while(millis() < endTime)_loop();
+}
+
+void _loop(){
+}
+
